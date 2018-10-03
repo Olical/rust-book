ed7a5ac239d8adf9020272e5386c779c797291fe
Oliver Caldwell <olliec87@gmail.com>
2018-10-03 23:12:16 +0100

    Debug printing

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,6 +10,7 @@ fn main() {
         height: 50,
     };
 
+    println!("rect1 is {:?}", rect1);
     println!("The area of the rectangle is {} square pixels.",
              area(&rect1));
 }
