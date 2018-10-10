ba8e02b3232734b22ea394b097c727005aa0e3a0
Oliver Caldwell <olliec87@gmail.com>
2018-10-10 13:19:00 +0100

    Basic fs and module structure for minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,22 @@
+extern crate minigrep;
+
 use std::env;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[1];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
