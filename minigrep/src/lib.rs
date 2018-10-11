74e63976eb1fcbbe0f683292f7bb0500334c237f
Oliver Caldwell <olliec87@gmail.com>
2018-10-11 12:58:47 +0100

    Actually have working minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -24,9 +24,23 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -68,5 +82,16 @@ mod tests {
             String::from("poem.txt"),
         ]).unwrap();
         run(config).expect("couldn't run with config")
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
