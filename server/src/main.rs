6792b315f8ff1b6124b74a4538c6695139a54a50
Oliver Caldwell <olliec87@gmail.com>
2018-10-26 14:26:25 +0100

    Refactor and add 404

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,9 +16,16 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let get = b"GET / HTTP/1.1\r\n";
 
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
